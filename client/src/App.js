de53265eb39e124a488c1afeb86fc156506f39ec
RyanGuinchard <ryanguinchard9@hotmail.com>
2023-12-24 19:18:43 -0330

    Added basic darkmode functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import "./styles/DarkMode.css";
 import PetDropdown from "./components/PetDropdown";
 
 function App() {
+  // Setting up state for dark mode
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => {
+      document.body.classList.toggle('dark-mode', !prevMode);
+      return !prevMode;
+    });
+  };
+  
+
   return (
     <>
-      <div className="container">
+      <div className={`container ${darkMode ? 'dark-mode' : ''}`}>
         <h1>Safe Snacks For Special Pets</h1>
-        <PetDropdown/>
+        <PetDropdown />
+        <button onClick={toggleDarkMode}>
+          {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+        </button>
       </div>
     </>
   );
